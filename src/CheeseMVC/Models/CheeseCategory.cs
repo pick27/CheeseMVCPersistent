6ac6806f2c6996971c84d366777854b93282aedf
Scott Land <101.scott@gmail.com>
2017-04-09 19:02:23 -0500

    Part 2 not tested. But compiled. Wanting backup before I kill the laptop.

--- src/CheeseMVC/Models/CheeseCategory.cs
+++ src/CheeseMVC/Models/CheeseCategory.cs
@@ -8,7 +8,8 @@ namespace CheeseMVC.Models
     public class CheeseCategory
     {
         public int ID { get; set; }
-
         public string Name { get; set; }
+
+        public IList<Cheese> Cheeses { get; set; }
     }
 }
